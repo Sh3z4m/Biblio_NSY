2e3aedfad98042f2b976a8979d39d36c892bba3e
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-01-23 09:54:34 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,34 +17,7 @@ def all_books():
     conn.close()
     return render_template('display_all.html', data=data)
 
-# Emprunter
-@app.route('/', methods=['POST'])
-def emprunt():
-    # Récupération de l'ISBN envoyé par le formulaire
-    isbn = request.form['emprunt']
-    # Connexion à la base de données
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    # Vérification si le livre avec cet ISBN existe et récupération du stock
-    cursor.execute('SELECT nbre_exemplaires FROM livres WHERE isbn = ?', (isbn,))
-    livre = cursor.fetchone()
-    if livre:
-        nombre_de_livres = livre[0]
-        if nombre_de_livres > 0:
-            # Mise à jour : réduction du stock de 1
-            cursor.execute('UPDATE livres SET nombre_de_livres = nombre_de_livres - 1 WHERE isbn = ?', (isbn,))
-            conn.commit()
-            message = f"Livre avec ISBN {isbn} emprunté avec succès."
-        else:
-            message = f"Aucun exemplaire disponible pour l'ISBN {isbn}."
-    else:
-        message = f"Aucun livre trouvé avec l'ISBN {isbn}."
 
-    # Fermeture de la connexion
-    conn.close()
-
-    # Retourne un template ou un message de confirmation
-    return render_template('display_all.html', message=message)
 
 # Afficher tous les livres
 @app.route('/enregistrer/')
